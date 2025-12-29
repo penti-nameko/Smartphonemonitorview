05b4da065d487336d6c3230e14ee61112edb0450
ぺんち <namekotoyuki100@gmail.com>
2025-12-29 16:48:09 +0900

    Implement screen capture functionality in MainActivity

--- android-mirror/app/src/main/java/com/example/mirror/MainActivity.java
+++ android-mirror/app/src/main/java/com/example/mirror/MainActivity.java
@@ -1,1 +1,31 @@
+package com.example.mirror;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.media.projection.MediaProjectionManager;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    private static final int REQ = 1;
+
+    @Override
+    protected void onCreate(Bundle b) {
+        super.onCreate(b);
+
+        MediaProjectionManager mpm =
+                (MediaProjectionManager) getSystemService(MEDIA_PROJECTION_SERVICE);
+        startActivityForResult(mpm.createScreenCaptureIntent(), REQ);
+    }
+
+    @Override
+    protected void onActivityResult(int req, int res, Intent data) {
+        if (req == REQ && res == RESULT_OK) {
+            Intent i = new Intent(this, ScreenService.class);
+            i.putExtra("code", res);
+            i.putExtra("data", data);
+            startService(i);
+            finish();
+        }
+    }
+}
